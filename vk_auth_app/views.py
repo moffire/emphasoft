6440b8a4d335a1eb5fa359ff738375b841f1abd8
AlexeyDudarev <alexeydudarev.spb@gmail.com>
2020-10-13 21:20:08 +0300

    added authentication via vk

--- vk_auth_app/views.py
+++ vk_auth_app/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from social_django.models import UserSocialAuth
+from vk_api import VkApi
+
+
+@login_required
+def index(request):
+    user_id = request.user.id
+    try:
+        user = UserSocialAuth.objects.get(user=user_id)
+    except UserSocialAuth.DoesNotExist:
+        return render(request, 'auth.html')
+
+    user_token = user.access_token
+    session = VkApi(token=user_token)
+    api = session.get_api()
+    friends = api.friends.get(count=6, fields=['first_name, last_name, photo_200'])
+    return render(request, 'index.html', {'friends': friends['items']})
